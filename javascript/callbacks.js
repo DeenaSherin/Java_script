f77791a521b653cd480db1a367abbd47b3dc27ae
Sherin <sdeenasherin@gmail.com>
2024-01-23 10:47:18 +0530

    javascript

--- javascript/callbacks.js
+++ javascript/callbacks.js
@@ -11,7 +11,7 @@ function second(){
     console.log("Welcome")
 }
 first("Sherin",second)
-*/
+
 function first (name,callback){
     setTimeout(function(){
         console.log("Get lost")
@@ -23,3 +23,29 @@ function second(a,b){
     console.log(a+b)
 }
 first("Sherin",function(){second(10,20)})
+
+
+//Example
+function prize(a,callback){
+    setTimeout(function(){
+        console.log(a)
+        callback()
+},1000)
+}
+
+function noprize(){
+    console.log("You didnot receive any prize")
+}
+prize("You have won the first prize",noprize)
+
+*/
+function me (d,callback){
+    setTimeout(function(){console.log(d)
+    callback()},500)
+}
+
+
+function u(a,b){
+    console.log(a,"",b)
+}
+me("You have chosen me",function(){u("wh?","How?")})
--- javascript/promise.js
+++ javascript/promise.js
@@ -1,4 +1,5 @@
-function isAuthenticated(){
+
+/*function isAuthenticated(){
     return Math.random()>1
 }
 function login(){
@@ -25,3 +26,35 @@ login().then(function(message){
 .catch(function(message){
     console.log(message)
 })
+*/
+function isLocationValid(loc){
+    var v="Melbourne"
+    if (v===loc){
+        return true
+    }
+    else{
+        return false
+    }
+}
+function location(){
+    return new Promise(function(response,errmessage){
+        setTimeout(function(){
+            if (isLocationValid){
+                return response("Location Found")
+
+            }
+            else{
+                return errmessage("Location not found")
+            }
+        },1000)
+    }
+    )
+
+}
+location().then(function(response){
+    console.log(response)
+})
+.catch(function(resquest){
+    console.log(resquest)
+})
+za
